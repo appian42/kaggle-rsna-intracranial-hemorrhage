fdc5465d4ea97920186f39ab1e090e2ba67ea73d
Walter Wiggins <walterfwiggins@gmail.com>
2022-12-16 08:30:46 -0500

    fixes Docker build

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ ENV NVIDIA_DISABLE_REQUIRE=1
 RUN mkdir -p /setup
 COPY ./requirements.txt /setup
 WORKDIR /setup
-RUN pip install -r requirements.txt --no-cache-dir
+RUN pip install -r requirements.txt --no-cache-dir --use-feature=2020-resolver
--- bin/ensemble.sh
+++ bin/ensemble.sh
@@ -9,7 +9,7 @@ test100="[\
     ['./model/model100/fold7_ep2_test_tta5.pkl', './model/model100/fold7_ep3_test_tta5.pkl'],\
 ]"
 
-python -m src.postprocess.ensemble --inputs "${test100}" --output ./inputs/test_predictions.csv
+python -m src.postprocess.ensemble --inputs "${test100}" --output ./input/test_predictions.csv
 
 # python -m src.postprocess.make_submission --inputs "['./meta/meta100_lgb.pkl', './meta/meta100_cat.pkl', './meta/meta100_xgb.pkl']" --output ./submission/sub001.csv
 #kaggle competitions submit rsna-intracranial-hemorrhage-detection -m "" -f ./submission/sub001.csv
